dd0ad98a0f2b1dfb7f57b7536feb6ad3e164fa9e
Cody Lee <132834285+Lee-Cody@users.noreply.github.com>
2024-03-13 22:35:42 -0400

    Delete jars/Lib/FInCH directory

--- jars/Lib/FInCH/__init__.py
+++
@@ -1,7 +0,0 @@
-#https://github.com/Lee-Cody/FInCH
-#Copyright 2024 Cody Lee
-#Licensed under GPL-3.0
-#See FInCH_.py for colour deconvolution attribution
-__all__ = ["Interface", "DataManager","GridGen","Utils","FInCHPlugins"]
-
-
